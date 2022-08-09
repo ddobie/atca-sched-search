07ae8b3688849bceb1044ef206721ed3a01b109c
Dougal Dobie <ddobie@users.noreply.github.com>
2022-08-09 16:47:21 +1000

    Update search_sched.py

--- search_sched.py
+++ search_sched.py
@@ -35,11 +35,23 @@ def load_schedule(filepath):
         
     return df
   
-def apply_obs_constraints(df,good_arrays=['6A','6B','6C','6D','1.5A','1.5B','1.5C','1.5D'], greentime=True):
+def apply_obs_constraints(df,
+                          good_arrays,
+                          greentime=True,
+                          start_dt=None,
+                          end_dt=None
+                          ):
+ 
     array_constraint = df['array'].isin(good_arrays)
     greentime_constraint = df['className']=='Green Time'
-
-    good_df = df[array_constraint & greentime_constraint]
+    
+    overall_constraint = array_constraint & greentime_constraint
+    if start_dt is not None:
+        overall_constraint = overall_constraint & (df['end'] > start_dt)
+    if end_dt is not None:
+        overall_constraint = overall_constraint & (df['start'] < end_dt)
+    
+    good_df = df[overall_constraint]
     
     return good_df
 
@@ -66,7 +78,7 @@ def get_overlaps(df, lst_range):
         print()
     
     
-def get_AltAz(df,coord, horizon=12*u.deg):
+def get_AltAz(df, coord, min_obs_length, horizon=12*u.deg):
     obs_frame_start = AltAz(obstime=df['start'], location=ATCA)
     obs_frame_end = AltAz(obstime=df['end'], location=ATCA)
     
@@ -92,7 +104,7 @@ def get_AltAz(df,coord, horizon=12*u.deg):
     df['obs_end'] = latest_end
     df['obs_length'] = df['obs_end']-df['obs_start']
     
-    lengthy = df['obs_length'] >= pd.Timedelta(6,'h')
+    lengthy = df['obs_length'] >= min_obs_length
     obs_blocks = df[lengthy][['obs_start','obs_end', 'obs_length','array']]
     num_obs = len(obs_blocks)
     
@@ -104,28 +116,49 @@ def get_AltAz(df,coord, horizon=12*u.deg):
     return False
 
 
-def analyse_sources(filename, sched_filepath=None, semester=None):
-    sources = pd.read_csv(filename)#, encoding = "ISO-8859-1")
-    
-    print(sources)
-    units = (u.deg, u.deg)
-    if ':' in sources['ra']:
-        units = (u.hourangle, u.deg)
-    sc = SkyCoord(sources['ra'],sources['dec'], unit=units)
+def analyse_sources(sc=None,
+                    filename=None,
+                    sched_filepath=None,
+                    semester=None,
+                    good_arrays=['6A','6B','6C','6D','1.5A','1.5B','1.5C','1.5D'],
+                    start_dt=pd.Timestamp("today", tz='UTC'),
+                    end_dt=None,
+                    min_obs_length=pd.Timedelta(1,'h')
+                    ):
+    if sc is not None and filename is not None:
+        print("Received skycoords and catalogue - pick one!")
+    elif sc is None and filename is None:
+        print("Must supply a skycoord or catalogue!")
+        
+    if filename is not None:
+        sources = pd.read_csv(filename)
+        units = (u.deg, u.deg)
+        if ':' in sources['ra'].iloc[0]:
+            units = (u.hourangle, u.deg)
+        sc = SkyCoord(sources['ra'],sources['dec'], unit=units)
     
     if sched_filepath is None:
         sched_filepath = get_schedule(semester)
     full_schedule = load_schedule(sched_filepath)
-    good_schedule = apply_obs_constraints(full_schedule)
+    good_schedule = apply_obs_constraints(full_schedule,
+                                          good_arrays,
+                                          start_dt,
+                                          end_dt)
     
     num_observable = 0
     for i,coord in enumerate(sc):
-        print(sources['name'].iloc[i])
-        observable = get_AltAz(good_schedule,coord)
+        if filename is not None:
+            print(sources['name'].iloc[i])
+        else:
+            print(f"Source {i}")
+        observable = get_AltAz(good_schedule,coord, min_obs_length)
         print()
         
         num_observable += observable
     print('{} sources are observable'.format(num_observable))
-        
-get_schedule('2021Apr')
-analyse_sources('sn2012dy.csv', semester='2020Apr')
+
+
+#analyse_sources(filename='sn2012dy.csv', semester='2022Apr')
+
+sc = SkyCoord(['06:23:09.28'], ['-04:56:22.79'], unit=(u.hourangle, u.deg))
+analyse_sources(sc, semester='2022Apr', good_arrays=['H168'])
